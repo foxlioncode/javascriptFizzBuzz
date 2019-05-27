3c694b7b2a38b357073ebae065583aabbe1346e7
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-05-27 13:53:01 -0400

    fizzBuzz, fizz, buzz logs all added. logs correctly to console.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,18 @@
 const fizzBuzz = (fizz, buzz) => {
 
     for (let i = 1; i <= 100; i++) {
-        console.log(i);
+        
+        if (i % fizz === 0 && i % buzz === 0) {
+            console.log('fizzBuzz');
+        } else if (i % fizz === 0) {
+            console.log('fizz');
+        } else if (i % buzz === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+        
     }
+};
 
-}
-
-fizzBuzz()
+fizzBuzz(5,3);
